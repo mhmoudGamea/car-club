b4431760ae2933b5782480a3563b552274319b21
Mhmoud gamea <mhmoudgamea@gmail.com>
2023-04-16 11:58:39 +0200

    car center item comitted

--- lib/features/services/presentation/views/widgets/car_center_item_body.dart
+++ lib/features/services/presentation/views/widgets/car_center_item_body.dart
@@ -1,110 +1,19 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:car_club/features/services/data/models/car_center_model.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../../../../core/constants.dart';
+import '../../../../../core/utils/assets.dart';
+import '../../../../../core/utils/styles.dart';
 
 class ItemViewBody extends StatelessWidget {
-  const ItemViewBody({Key? key, required this.carCenterModel }) : super(key: key);
+  const ItemViewBody({Key? key, required this.carCenterModel})
+      : super(key: key);
   static const rn = '/ItemViewBody';
   final CarCenterModel carCenterModel;
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return Container(
-      decoration: BoxDecoration(
-        color: greyColor.withOpacity(0.4),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        children: [
-          Stack(
-            children: const [
-              SizedBox(
-                height: 130,
-                width: double.infinity,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10)),
-                  child: Image(image: AssetImage(ford),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Car name',
-                      style: Styles.titleSmall.copyWith(
-                        color: Colors.black,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-
-                  ],
-                ),
-                const Divider(
-                  color: Colors.black45,
-                  // thickness: 1,
-                ),
-                const SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        const FaIcon(
-                          FontAwesomeIcons.bolt,
-                          color: Colors.black54,
-                          size: 19,
-                        ),
-                        const SizedBox(width: 5),
-                        Text(
-                          ' hp',
-                          style:
-                          Styles.titleSmall.copyWith(color: Colors.black54),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const FaIcon(
-                          FontAwesomeIcons.gaugeHigh,
-                          color: Colors.black54,
-                          size: 19,
-                        ),
-                        const SizedBox(width: 5),
-                        Text(
-                          '15 km/h',
-                          style:
-                          Styles.titleSmall.copyWith(color: Colors.black54),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const FaIcon(
-                          FontAwesomeIcons.dollarSign,
-                          color: Colors.black54,
-                          size: 19,
-                        ),
-                        const SizedBox(width: 5),
-                        Text(
-                          '50',
-                          style:
-                          Styles.titleSmall.copyWith(color: Colors.black54),
-                        ),
-                      ],
-                    ),
-                  ],
-=======
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: ClipRRect(
@@ -118,20 +27,21 @@ class ItemViewBody extends StatelessWidget {
             child: Column(
               children: [
                 Container(
-                  padding: const EdgeInsets.only(top: 10.0,left: 10,right: 10),
+                  padding:
+                      const EdgeInsets.only(top: 10.0, left: 10, right: 10),
                   height: 250,
                   child: Container(
                     clipBehavior: Clip.antiAliasWithSaveLayer,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10)
-                    ),
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(10)),
                     child: CachedNetworkImage(
                       imageUrl: carCenterModel.images[0],
-                      errorWidget: (context, url, error) =>  const Image(image: AssetImage(whiteImage)),
-                      placeholder: (context, url) => const Center(child : CircularProgressIndicator()),
+                      errorWidget: (context, url, error) =>
+                          const Image(image: AssetImage(whiteImage)),
+                      placeholder: (context, url) =>
+                          const Center(child: CircularProgressIndicator()),
                       width: double.infinity,
                       fit: BoxFit.fill,
-
                     ),
                   ),
                 ),
@@ -166,8 +76,8 @@ class ItemViewBody extends StatelessWidget {
                               const SizedBox(width: 5),
                               Text(
                                 '${carCenterModel.time} minutes',
-                                style:
-                                Styles.titleSmall.copyWith(color: Colors.black54),
+                                style: Styles.titleSmall
+                                    .copyWith(color: Colors.black54),
                               ),
                             ],
                           ),
@@ -181,14 +91,9 @@ class ItemViewBody extends StatelessWidget {
                               const SizedBox(width: 5),
                               Text(
                                 'open',
-                                style:
-                                Styles.titleSmall.copyWith(color: Colors.green),
+                                style: Styles.titleSmall
+                                    .copyWith(color: Colors.green),
                               ),
-                              // Text(
-                              //   'closed',
-                              //   style:
-                              //   Styles.titleSmall.copyWith(color: Colors.red),
-                              // ),
                             ],
                           ),
                           Row(
@@ -201,8 +106,8 @@ class ItemViewBody extends StatelessWidget {
                               const SizedBox(width: 5),
                               Text(
                                 '${carCenterModel.distance.ceil()} km',
-                                style:
-                                Styles.titleSmall.copyWith(color: Colors.black54),
+                                style: Styles.titleSmall
+                                    .copyWith(color: Colors.black54),
                               ),
                             ],
                           ),
@@ -210,16 +115,11 @@ class ItemViewBody extends StatelessWidget {
                       ),
                     ],
                   ),
->>>>>>> 4fc29250da27454e50c6ff9216c4c83f57288dd8
                 ),
               ],
             ),
           ),
-<<<<<<< HEAD
-        ],
-=======
         ),
->>>>>>> 4fc29250da27454e50c6ff9216c4c83f57288dd8
       ),
     );
   }
