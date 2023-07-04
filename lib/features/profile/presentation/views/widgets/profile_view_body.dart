6e370d9feebeec0f95badcab71e17c1322c6597f
Mhmoud gamea <mhmoudgamea@gmail.com>
2023-07-04 22:31:24 +0300

    update profile part one

--- lib/features/profile/presentation/views/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/views/widgets/profile_view_body.dart
@@ -1,12 +1,7 @@
-import 'dart:io';
-
-import 'package:car_club/features/profile/presentation/model_view/profile_cubit.dart';
-import 'package:car_club/features/profile/presentation/views/widgets/tabs/sells_view.dart';
+import 'package:car_club/features/profile/presentation/views/widgets/user_profile_information.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../core/constants.dart';
 
 //import '../../model_view/profile_cubit.dart';
@@ -22,21 +17,14 @@ class ProfileViewBody extends StatelessWidget {
     const Tab(
       icon: Icon(
         Icons.home,
-        color: greyColor,
+        color: blackColor,
       ),
     ),
     // fav tab
     const Tab(
       icon: Icon(
         Icons.favorite,
-        color: greyColor,
-      ),
-    ),
-    // sells tab
-    const Tab(
-      icon: Icon(
-        FontAwesomeIcons.cartArrowDown,
-        color: greyColor,
+        color: blackColor,
       ),
     ),
   ];
@@ -47,75 +35,13 @@ class ProfileViewBody extends StatelessWidget {
     const FeedView(),
     // fav view
     const FavView(),
-    // sells view
-    const SellsView(),
   ];
 
   @override
   Widget build(BuildContext context) {
-    final data = BlocProvider.of<ProfileCubit>(context);
     return ListView(
       children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              child: Stack(
-                alignment: AlignmentDirectional.bottomEnd,
-                children: [
-                  Container(
-                    height: 135,
-                    width: 135,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          // TODO: need to ....
-                          image: NetworkImage(user.profileImage!),
-                          fit: BoxFit.cover),
-                      shape: BoxShape.circle,
-                      color: greyColor,
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 10,
-                    right: 5,
-                    child: Container(
-                      width: 30,
-                      height: 30,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: mintGreen.withOpacity(0.5)),
-                      child: IconButton(
-                        onPressed: () async {
-                          File? pickedFileImage = await data.pickProfileImage();
-                          if (pickedFileImage != null) {
-                            await data.uploadPickedImage(pickedFileImage);
-                          }
-                        },
-                        icon: const Icon(FontAwesomeIcons.camera, size: 18),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Text(
-              'name',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'phone',
-                  style:
-                      TextStyle(color: babyBlue, fontWeight: FontWeight.bold),
-                ),
-              ],
-            )
-          ],
-        ),
+        const UserProfileInformation(),
         //tab bar
         TabBar(
           padding: const EdgeInsets.symmetric(horizontal: 20),
---
+++ lib/features/profile/presentation/views/widgets/user_profile_information.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+
+import 'package:car_club/core/utils/styles.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/constants.dart';
+import '../../../data/models/user_model.dart';
+import '../../model_view/profile_cubit.dart';
+
+class UserProfileInformation extends StatelessWidget {
+  const UserProfileInformation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final data = BlocProvider.of<ProfileCubit>(context);
+    final firebase = FirebaseFirestore.instance.collection('users').snapshots();
+    return StreamBuilder<QuerySnapshot>(
+      stream: firebase,
+      builder: ((context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 100, horizontal: 180),
+              child: const CircularProgressIndicator());
+        }
+        UserModel userModel = data.getUserFromSnapshot(snapshot.data);
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Stack(
+                alignment: AlignmentDirectional.bottomEnd,
+                children: [
+                  Container(
+                    height: 135,
+                    width: 135,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(userModel.profileImage!),
+                          fit: BoxFit.cover),
+                      shape: BoxShape.circle,
+                      color: greyColor,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    right: 5,
+                    child: Container(
+                      width: 35,
+                      height: 35,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: greyColo3.withOpacity(0.5)),
+                      child: IconButton(
+                        constraints: const BoxConstraints(),
+                        padding: EdgeInsets.zero,
+                        onPressed: () async {
+                          File? pickedFileImage = await data.pickProfileImage();
+                          if (pickedFileImage != null) {
+                            await data.uploadPickedImage(pickedFileImage);
+                          }
+                        },
+                        icon: const Icon(FontAwesomeIcons.camera, size: 18),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              userModel.name,
+              style: Styles.title15.copyWith(color: blackColor),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              userModel.phone,
+              style: Styles.title14.copyWith(color: Colors.grey),
+            ),
+          ],
+        );
+      }),
+    );
+  }
+}
